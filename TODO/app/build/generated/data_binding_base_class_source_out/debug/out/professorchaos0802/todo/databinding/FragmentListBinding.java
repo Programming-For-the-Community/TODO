d01a1a3f82debc5109806c45eeba05c4222239c1
ProfessorChaos0802 <1822rhit@pm.me>
2022-09-25 23:25:17 -0400

    Finished List fragment layout, started working on recycler logic

--- TODO/app/build/generated/data_binding_base_class_source_out/debug/out/professorchaos0802/todo/databinding/FragmentListBinding.java
+++ TODO/app/build/generated/data_binding_base_class_source_out/debug/out/professorchaos0802/todo/databinding/FragmentListBinding.java
@@ -4,25 +4,48 @@ package professorchaos0802.todo.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 import professorchaos0802.todo.R;
 
 public final class FragmentListBinding implements ViewBinding {
   @NonNull
-  private final ConstraintLayout rootView;
+  private final RelativeLayout rootView;
 
-  private FragmentListBinding(@NonNull ConstraintLayout rootView) {
+  @NonNull
+  public final RecyclerView listItemRecycler;
+
+  @NonNull
+  public final EditText listTitle;
+
+  @NonNull
+  public final Button listUncheckAll;
+
+  @NonNull
+  public final EditText newItem;
+
+  private FragmentListBinding(@NonNull RelativeLayout rootView,
+      @NonNull RecyclerView listItemRecycler, @NonNull EditText listTitle,
+      @NonNull Button listUncheckAll, @NonNull EditText newItem) {
     this.rootView = rootView;
+    this.listItemRecycler = listItemRecycler;
+    this.listTitle = listTitle;
+    this.listUncheckAll = listUncheckAll;
+    this.newItem = newItem;
   }
 
   @Override
   @NonNull
-  public ConstraintLayout getRoot() {
+  public RelativeLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +66,38 @@ public final class FragmentListBinding implements ViewBinding {
 
   @NonNull
   public static FragmentListBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.list_item_recycler;
+      RecyclerView listItemRecycler = ViewBindings.findChildViewById(rootView, id);
+      if (listItemRecycler == null) {
+        break missingId;
+      }
 
-    return new FragmentListBinding((ConstraintLayout) rootView);
+      id = R.id.list_title;
+      EditText listTitle = ViewBindings.findChildViewById(rootView, id);
+      if (listTitle == null) {
+        break missingId;
+      }
+
+      id = R.id.list_uncheck_all;
+      Button listUncheckAll = ViewBindings.findChildViewById(rootView, id);
+      if (listUncheckAll == null) {
+        break missingId;
+      }
+
+      id = R.id.new_item;
+      EditText newItem = ViewBindings.findChildViewById(rootView, id);
+      if (newItem == null) {
+        break missingId;
+      }
+
+      return new FragmentListBinding((RelativeLayout) rootView, listItemRecycler, listTitle,
+          listUncheckAll, newItem);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
