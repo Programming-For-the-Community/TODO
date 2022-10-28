570e80a29204fcbcf03624b4e10fb1ab032296f8
ProfessorChaos0802 <1822rhit@pm.me>
2022-10-27 22:46:27 -0400

    Started converting to Compose by converting colors and building lightBlueTheme

---
+++ TODO/app/src/main/java/professorchaos0802/todo/theme/Color.kt
@@ -0,0 +1,80 @@
+package professorchaos0802.todo.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val Black = Color(0xFF000000)
+val DarkGary = Color(0xFF3E3E3E)
+val LightGray = Color(0xFFA4A4A4)
+
+// Blues
+val MidnightBlue = Color(0xFF03045E)
+val DarkCornflowerBlue = Color(0xFF023E8A)
+val StarCommandBlue = Color(0xFF0077B6)
+val BlueGreen = Color(0xFF0096C7)
+val PacificBlue = Color(0xFF00B4D8)
+val SkyBlueCrayola = Color(0xFF48CAE4)
+val MiddleBlue = Color(0xFF90E0Ef)
+val BlizzardBlue1 = Color(0xFFA6E6F2)
+val BlizzardBlue2 = Color(0xFFB8EBF5)
+val PowderBlue = Color(0xFFC6EFF7)
+
+// Greens
+val DarkJungleGreen = Color(0xFF081C15)
+val PhthaloGreen = Color(0xFF123024)
+val MsuGreen = Color(0xFF1B4332)
+val BottleGreen = Color(0xFF2D6A4F)
+val IlluminatingEmerald = Color(0xFF40916C)
+val Mint = Color(0xFF52B788)
+val OceanGreen = Color(0xFF74C69D)
+val TurquoiseGreen1 = Color(0xFF95D5B2)
+val TurquoiseGreen2 = Color(0xFFB7E4C7)
+val Nyanza = Color(0xFFD8F3DC)
+
+// Reds
+val BloodRed = Color(0xFF680B00)
+val BarnRedDark = Color(0xFF750D00)
+val BarnRedLight = Color(0xFF820E00)
+val DarkRed = Color(0xFF931000)
+val Rufous = Color(0xFFA31100)
+val InternationalOrangeEngineering = Color(0xFFB81300)
+val RossoCorsa1 = Color(0xFFCC1500)
+val RossoCorsa2 = Color(0xFFDD1700)
+val Red = Color(0xFFEE1800)
+val Scarlet = Color(0xFFFF1A00)
+
+// Oranges
+val Xiketic = Color(0xFF03071E)
+val DarkSienna = Color(0xFF370617)
+val RoseWood = Color(0xFF6A040F)
+val DarkRedOrange = Color(0xFF9D0208)
+val RossoCorsaOrange = Color(0xFFD00000)
+val Vermilion = Color(0xFFCD2F02)
+val Persimmon = Color(0xFFE85D04)
+val Tangerine = Color(0xFFF48C06)
+val OrangeWeb = Color(0xFFFAA307)
+val SelectiveYellow = Color(0xFFFFBA08)
+
+// Pinks
+val Claret1 = Color(0xFF590D22)
+val Claret2 = Color(0xFF800F2F)
+val BigDipOruby = Color(0xFFA4133C)
+val BrightMaroon = Color(0xFFC9184A)
+val FieryRose = Color(0xFFFF4D6D)
+val UltraRed = Color(0xFFFF758F)
+val SalmonPink = Color(0xFFFF8FA3)
+val CherryBlossomPink = Color(0xFFFFB3C1)
+val Pink = Color(0xFFFFCCD5)
+val LavenderBlush = Color(0xFFFFF0F3)
+
+// Purples
+val BlueVioletColorWheel1 = Color(0xFF461873)
+val BlueVioletColorWheel2 = Color(0xFF58148E)
+val Purple = Color(0xFF6910A8)
+val DarkViolet = Color(0xFF8C07DD)
+val PurpleX11 = Color(0xFF9F21E3)
+val ElectricPurple = Color(0xFFB333E9)
+val MediumOrchid = Color(0xFFCB5DF1)
+val BrightLilac = Color(0xFFDC93F6)
+val Mauve = Color(0xFFEABFFA)
+val PalePurplePantone = Color(0xFFF7EBFD)
---
+++ TODO/app/src/main/java/professorchaos0802/todo/theme/Theme.kt
@@ -0,0 +1,69 @@
+package professorchaos0802.todo.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+val lightBluePalette = lightColorScheme(
+    background = PowderBlue,
+    primary = StarCommandBlue,
+    onPrimary = MiddleBlue,
+    primaryContainer = PacificBlue,
+    onPrimaryContainer = DarkCornflowerBlue,
+    secondary = MidnightBlue,
+    onSecondary = BlizzardBlue1,
+    secondaryContainer = PacificBlue,
+    onSecondaryContainer = DarkCornflowerBlue,
+    tertiary = DarkCornflowerBlue,
+    onTertiary = SkyBlueCrayola,
+    tertiaryContainer = BlizzardBlue2,
+    onTertiaryContainer = BlueGreen,
+    surface = DarkCornflowerBlue,
+    onSurface = SkyBlueCrayola
+)
+
+val darkBluePalette = darkColorScheme()
+
+val lightGreenPalette = lightColorScheme()
+val darkGreenPalette = darkColorScheme()
+
+val lightRedPalette = lightColorScheme()
+val darkRedPalette = darkColorScheme()
+
+val lightOrangePalette = lightColorScheme()
+val darkOrangePalette = darkColorScheme()
+
+val lightPinkPalette = lightColorScheme()
+val darkPinkPalette = darkColorScheme()
+
+val lightPurplePalette = lightColorScheme()
+val darkPurplePalette = darkColorScheme()
+
+@Composable
+fun TodoTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    color: String
+){
+    if(darkTheme){
+        when(color){
+            "Blue" -> darkBluePalette
+            "Green" -> darkGreenPalette
+            "Red" -> darkRedPalette
+            "Orange" -> darkOrangePalette
+            "Pink" -> darkPinkPalette
+            "Purple" -> darkPurplePalette
+            else -> darkBluePalette
+        }
+    }else{
+        when(color){
+            "Blue" -> lightBluePalette
+            "Green" -> lightGreenPalette
+            "Red" -> lightRedPalette
+            "Orange" -> lightOrangePalette
+            "Pink" -> lightPinkPalette
+            "Purple" -> lightPurplePalette
+            else -> lightBluePalette
+        }
+    }
+}
